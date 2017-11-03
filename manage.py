6fa8ac5de8d71a364526639cf979fad903ed88d2
feixiangingk <feixiangingk@163.com>
2017-11-03 15:50:56 +0800

    修改 manage.py

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import sys
-import time
+from datetime import date
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_rest.settings")
